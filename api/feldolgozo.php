60da91ce756f795f458f4e838b7b059cf4d54120
Pósán Róbert <posanrobi@gmail.com>
2023-11-02 00:49:48 +0100

    debugging

--- api/feldolgozo.php
+++ api/feldolgozo.php
@@ -114,8 +114,8 @@ if (file_exists($filename)) {
 
 
 function getFillColor($inputUsername) {
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    
+    //$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
     $dotenv->load();
 
     $servername = $_ENV["DB_SERVERNAME"];
