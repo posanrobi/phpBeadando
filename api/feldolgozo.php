8ab056cdd6cd7d3e135436f35bd99f72c302708f
Pósán Róbert <posanrobi@gmail.com>
2023-11-02 12:30:09 +0100

    debugging

--- api/feldolgozo.php
+++ api/feldolgozo.php
@@ -116,10 +116,16 @@ function getFillColor($inputUsername) {
 
     $dotenv->load();
 
-    $servername = $_ENV["DB_SERVERNAME"];
+    /* $servername = $_ENV["DB_SERVERNAME"];
     $username = $_ENV["DB_USERNAME"];
     $password = $_ENV["DB_PASSWORD"];
-    $dbname = $_ENV["DB_NAME"];
+    $dbname = $_ENV["DB_NAME"]; */
+
+    $servername = getenv("DB_SERVERNAME");
+    $username = getenv("DB_USERNAME");
+    $password = getenv("DB_PASSWORD");
+    $dbname = getenv("DB_NAME");
+
 
     $conn = new mysqli($servername, $username, $password, $dbname);
 
